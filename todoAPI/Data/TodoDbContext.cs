46a71071f7a3c3214f5613f1b3c126953f541296
daviidx23 <davidmas.sm@gmail.com>
2024-07-18 23:02:34 +0200

    Creada la base de datos

---
+++ todoAPI/Data/TodoDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using todoAPI.Entities;
+
+namespace todoAPI.Data
+{
+    public class TodoDbContext : DbContext
+    {
+        private readonly IConfiguration configuration;
+
+        public DbSet<TodoTask> Tasks {  get; set; }
+
+        public TodoDbContext(IConfiguration configuration)
+        {
+            this.configuration = configuration;   
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(configuration.GetConnectionString("PostgreSqlConnection"));
+        }
+    }
+}
---
+++ todoAPI/Entities/TodoTask.cs
@@ -0,0 +1,9 @@
+﻿namespace todoAPI.Entities
+{
+    public class TodoTask
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string? Description { get; set; }
+    }
+}
